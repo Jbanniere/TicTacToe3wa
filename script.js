96bebb44c9794ebc7e87be7aaf7ee7d95afa9d14
Jbanniere <bannier.julie91@gmail.com>
2022-12-19 11:22:41 +0000

    remove du onClick et création des winCase

--- script.js
+++ script.js
@@ -8,43 +8,61 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const gameCase = document.querySelector('.container')
     const box = document.querySelectorAll('.box')
     let player = document.getElementById('player')
     const restart = document.querySelector('button')
 
-    const game = ["", "", ""]
+    let gameCase = ["", "", ""]
     let nbClick = 0
-    
+    let tour = 0
 
-   /**
-    * 
-    * Fonction pour afficher dans le HTML
-    * 
-    */
-    
-    for (let i = 0; i < box.length; i++) {
-        box[i].addEventListener('click', () => {
-            nbClick++
-            if (nbClick % 2 === 1) {
-                player.innerText = "PLayer O à vous de jouer !"
-                box[i].innerText = "O"
-                box[i].removeEventListener()
-            }
-            else if (nbClick % 2 === 0) {
-                player.innerText = "PLayer X à vous de jouer !"
-                box[i].innerText = "X"
-                box[i].removeEventListener()
-            }
-           
-        })
-    }
-    
+
     /**
-    * 
-    * Function pour savoir qui a gagné
-    * 
-    */
-    
+     * 
+     * Fonction pour afficher dans le HTML
+     * 
+     */
+    const playerTurn = (i) => {
+        nbClick++
+        if (nbClick % 2 === 1) {
+            player.innerText = "PLayer X à vous de jouer !"
+            box[i].innerText = "O"
+        }
+        else if (nbClick % 2 === 0) {
+            player.innerText = "PLayer O à vous de jouer !"
+            box[i].innerText = "X"
+        }
+        box[i].removeEventListener('click', playerTurn())
+        console.log(nbClick)
+    }
+
+
+    for (let i = 0; i < box.length; i++) {
+        box[i].addEventListener('click', () => playerTurn(i))
+    }
+
+    /**
+     * 
+     * Function pour savoir qui a gagné
+     * 
+     */
+
+    let winGame = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    const checkWin = () => {
+        for (let i = 0 ; i < gameCase.length ; i++) {
+            
+        }
+    }
+
 
 })
