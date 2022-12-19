b97a46a50154ce3acea05acf1ce4f577eb059117
Jbanniere <bannier.julie91@gmail.com>
2022-12-19 13:56:23 +0000

    Identification du winner et affichage du score

--- script.js
+++ script.js
@@ -8,141 +8,114 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-            const box = document.querySelectorAll('.box')
-            let player = document.getElementById('player')
-            const restartBtn = document.querySelector('button')
-            const id1 = document.getElementById('0-1')
-            let scoreX = document.getElementById('scoreX')
-            let scoreO = document.getElementById('scoreO')
+    const box = document.querySelectorAll('.box')
+    let player = document.getElementById('player')
+    const restartBtn = document.querySelector('button')
+    const id1 = document.getElementById('0-1')
+    let boxScoreX = document.getElementById('scoreX')
+    let boxScoreO = document.getElementById('scoreO')
+    let scoreX = 0
+    let scoreO = 0
 
-            let nbClick = 0
-            let tour = 0
+    let tour = 0
 
+
+    let winGame = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    /**
+     * 
+     * Function pour savoir qui a gagné
+     * 
+     */
+    const check = () => {
+        for (let i = 0; i < winGame.length; i++) {
             let gameCase = ["", "", ""]
-            let winGame = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-            ]
 
-
-            /**
-             * 
-             * Fonction pour afficher dans le HTML
-             * 
-             */
-            const playerTurn = (element) => {
-                if (element.innerText === "") {
-                    nbClick++
-                    if (nbClick % 2 === 1) {
-                        player.innerText = "PLayer X à vous de jouer !"
-                        element.innerText = "O"
-
-                    }
-                    else if (nbClick % 2 === 0) {
-                        player.innerText = "PLayer O à vous de jouer !"
-                        element.innerText = "X"
-                    }
-                    console.log(element)
-                }
-
+            if (box[winGame[i][0]].innerText === "X") {
+                gameCase[0] = true
+            }
+            else if (box[winGame[i][0]].innerText === "O") {
+                gameCase[0] = false
+            }
+            if (box[winGame[i][1]].innerText === "X") {
+                gameCase[1] = true
+            }
+            else if (box[winGame[i][1]].innerText === "O") {
+                gameCase[1] = false
             }
 
-            box.forEach(element =>
-                element.addEventListener('click', () =>
-                    playerTurn(element)
-                )
-            )
+            if (box[winGame[i][2]].innerText === "X") {
+                gameCase[2] = true
+            }
+            else if (box[winGame[i][2]].innerText === "O") {
+                gameCase[2] = false
+            }
+            whoWin(gameCase[0], gameCase[1], gameCase[2])
+        }
+    }
+
+    const whoWin = (first, second, third) => {
+        if (first === true && second === true && third === true) {
+            scoreX++
+            boxScoreX.innerText = scoreX
+            alert('Player X vous marquez un point !')
+            
+        }
+
+        if (first === false && second === false && third === false) {
+            scoreO++
+            boxScoreO.innerText = scoreO
+            alert('Player O vous marquez un point !')
+        }
+    }
 
 
-            /**
-             * 
-             * Function pour savoir qui a gagné
-             * 
-             */
+    /**
+     * 
+     * Fonction pour afficher dans le HTML
+     * 
+     */
+    const playerTurn = (element) => {
+        if (element.innerText === "") {
+            tour++
+            if (tour % 2 === 1) {
+                player.innerText = "PLayer X à vous de jouer !"
+                element.innerText = "O"
 
-            const whoWin = () => {
-                    if (
-                        box[0].innerHTML === "X" &&
-                        box[1].innerHTML === "X" &&
-                        box[2].innerHTML === "X"
-                    ) {
-                        scoreX++
-                        return true
-                    }
-                    if (
-                        box[3].innerHTML === "X" &&
-                        box[4].innerHTML === "X" &&
-                        box[5].innerHTML === "X"
-                    ) {
-                        scoreX++
-                        return true
-                    }
-                    if (
-                        box[6].innerHTML === "X" &&
-                        box[7].innerHTML === "X" &&
-                        box[8].innerHTML === "X"
-                    ) {
-                        scoreX++
-                        return true
-                    }
-                    if (
-                        box[0].innerHTML === "X" &&
-                        box[3].innerHTML === "X" &&
-                        box[6].innerHTML === "X"
-                        ) {
-                        scoreX++
-                        return true
-                    }
-                    if (
-                        box[1].innerHTML === "X" &&
-                        box[4].innerHTML === "X" &&
-                        box[7].innerHTML === "X"
-                        ) {
-                        scoreX++
-                        return true
-                    }
-                    if (
-                        box[2].innerHTML === "X" &&
-                        box[5].innerHTML === "X" &&
-                        box[8].innerHTML === "X"
-                        ) {
-                        scoreX++
-                        return true
-                    }
-                     if (
-                        box[0].innerHTML === "X" &&
-                        box[4].innerHTML === "X" &&
-                        box[8].innerHTML === "X"
-                        ) {
-                        scoreX++
-                        return true
-                    }
-                     if (
-                        box[0].innerHTML === "X" &&
-                        box[4].innerHTML === "X" &&
-                        box[6].innerHTML === "X"
-                        ) {
-                        scoreX++
-                        return true
-                    }
+            }
+            else if (tour % 2 === 0) {
+                player.innerText = "PLayer O à vous de jouer !"
+                element.innerText = "X"
+            }
+            console.log(element)
+        }
+        check()
 
+    }
 
+    box.forEach(element =>
+        element.addEventListener('click', () =>
+            playerTurn(element)
+        )
+    )
 
-                            /**
-                             * 
-                             * Function pour restart le game
-                             * 
-                             */
-                            box.forEach(element =>
-                                restartBtn.addEventListener('click', () =>
-                                    element.innerText = " "
-                                )
-                            )
-
-                        })
+    /**
+     * 
+     * Function pour restart le game
+     * 
+     */
+    box.forEach(element =>
+        restartBtn.addEventListener('click', () =>
+            element.innerText = " "
+        )
+    )
+})
