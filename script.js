8934c114f0a722aeabdf990b4cf2f42f752ceef7
Jbanniere <bannier.julie91@gmail.com>
2022-12-19 13:21:34 +0000

    event restart OK et conditions de win pour X

--- script.js
+++ script.js
@@ -8,61 +8,141 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const box = document.querySelectorAll('.box')
-    let player = document.getElementById('player')
-    const restart = document.querySelector('button')
+            const box = document.querySelectorAll('.box')
+            let player = document.getElementById('player')
+            const restartBtn = document.querySelector('button')
+            const id1 = document.getElementById('0-1')
+            let scoreX = document.getElementById('scoreX')
+            let scoreO = document.getElementById('scoreO')
 
-    let gameCase = ["", "", ""]
-    let nbClick = 0
-    let tour = 0
+            let nbClick = 0
+            let tour = 0
 
-
-    /**
-     * 
-     * Fonction pour afficher dans le HTML
-     * 
-     */
-    const playerTurn = (i) => {
-        nbClick++
-        if (nbClick % 2 === 1) {
-            player.innerText = "PLayer X à vous de jouer !"
-            box[i].innerText = "O"
-        }
-        else if (nbClick % 2 === 0) {
-            player.innerText = "PLayer O à vous de jouer !"
-            box[i].innerText = "X"
-        }
-        box[i].removeEventListener('click', playerTurn())
-        console.log(nbClick)
-    }
+            let gameCase = ["", "", ""]
+            let winGame = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ]
 
 
-    for (let i = 0; i < box.length; i++) {
-        box[i].addEventListener('click', () => playerTurn(i))
-    }
+            /**
+             * 
+             * Fonction pour afficher dans le HTML
+             * 
+             */
+            const playerTurn = (element) => {
+                if (element.innerText === "") {
+                    nbClick++
+                    if (nbClick % 2 === 1) {
+                        player.innerText = "PLayer X à vous de jouer !"
+                        element.innerText = "O"
 
-    /**
-     * 
-     * Function pour savoir qui a gagné
-     * 
-     */
+                    }
+                    else if (nbClick % 2 === 0) {
+                        player.innerText = "PLayer O à vous de jouer !"
+                        element.innerText = "X"
+                    }
+                    console.log(element)
+                }
 
-    let winGame = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
+            }
 
-    const checkWin = () => {
-        for (let i = 0 ; i < gameCase.length ; i++) {
-            
-        }
-    }
+            box.forEach(element =>
+                element.addEventListener('click', () =>
+                    playerTurn(element)
+                )
+            )
 
 
-})
+            /**
+             * 
+             * Function pour savoir qui a gagné
+             * 
+             */
+
+            const whoWin = () => {
+                    if (
+                        box[0].innerHTML === "X" &&
+                        box[1].innerHTML === "X" &&
+                        box[2].innerHTML === "X"
+                    ) {
+                        scoreX++
+                        return true
+                    }
+                    if (
+                        box[3].innerHTML === "X" &&
+                        box[4].innerHTML === "X" &&
+                        box[5].innerHTML === "X"
+                    ) {
+                        scoreX++
+                        return true
+                    }
+                    if (
+                        box[6].innerHTML === "X" &&
+                        box[7].innerHTML === "X" &&
+                        box[8].innerHTML === "X"
+                    ) {
+                        scoreX++
+                        return true
+                    }
+                    if (
+                        box[0].innerHTML === "X" &&
+                        box[3].innerHTML === "X" &&
+                        box[6].innerHTML === "X"
+                        ) {
+                        scoreX++
+                        return true
+                    }
+                    if (
+                        box[1].innerHTML === "X" &&
+                        box[4].innerHTML === "X" &&
+                        box[7].innerHTML === "X"
+                        ) {
+                        scoreX++
+                        return true
+                    }
+                    if (
+                        box[2].innerHTML === "X" &&
+                        box[5].innerHTML === "X" &&
+                        box[8].innerHTML === "X"
+                        ) {
+                        scoreX++
+                        return true
+                    }
+                     if (
+                        box[0].innerHTML === "X" &&
+                        box[4].innerHTML === "X" &&
+                        box[8].innerHTML === "X"
+                        ) {
+                        scoreX++
+                        return true
+                    }
+                     if (
+                        box[0].innerHTML === "X" &&
+                        box[4].innerHTML === "X" &&
+                        box[6].innerHTML === "X"
+                        ) {
+                        scoreX++
+                        return true
+                    }
+
+
+
+                            /**
+                             * 
+                             * Function pour restart le game
+                             * 
+                             */
+                            box.forEach(element =>
+                                restartBtn.addEventListener('click', () =>
+                                    element.innerText = " "
+                                )
+                            )
+
+                        })
